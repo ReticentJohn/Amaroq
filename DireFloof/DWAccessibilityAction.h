1471cb21efef3253a9053fc585634cc1633101db
John Gabelmann <john@floof.dog>
2018-04-15 21:18:20 -0400

    Cleanup
    Signed-off-by: John Gabelmann <john@floof.dog>

--- DireFloof/DWAccessibilityAction.h
+++ DireFloof/DWAccessibilityAction.h
@@ -5,6 +5,9 @@
 //  Created by John Gabelmann on 4/15/18.
 //  Copyright © 2018 Keyboard Floofs. All rights reserved.
 //
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #import <UIKit/UIKit.h>
 #import "DWConstants.h"
